c29380111344035d1977ef2fb79563568229fbff
fardhan12 <fardhansaidinarasyid@gmail.com>
2023-11-24 19:55:57 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+  fontFamily: {
+    sans: ['arial', 'poppins', 'roboto'],
+  },
+
+  colors: {
+    transparent: 'transparent',
+    current: 'currentColor',
+    'white': '#ffffff',
+    'purple': '#3f3cbb',
+    'midnight': '#121063',
+    'metal': '#565584',
+    'tahiti': '#3ab7bf',
+    'silver': '#ecebff',
+    'bubble-gum': '#ff77e9',
+    'bermuda': '#78dcca',
+  },
+}
+
